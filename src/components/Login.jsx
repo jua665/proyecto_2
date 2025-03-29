36149ba9fe4e79fb365b6772c9d8675420547cf7
PabloCarranza31 <158582000+PabloCarranza31@users.noreply.github.com>
2025-03-29 16:28:38 -0600

    elimina cifrado

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,17 +17,6 @@ const loginValidationSchema = yup.object().shape({
     .required("*Campo Obligatorio"),
 });
 
-// Encriptado
-const caesarCipher = (str, shift) => {
-  return str.split('').map(char => {
-    if (char.match(/[a-z]/i)) {
-      const code = char.charCodeAt();
-      let base = code >= 65 && code <= 90 ? 65 : 97;
-      return String.fromCharCode(((code - base + shift) % 26) + base);
-    }
-    return char;
-  }).join('');
-};
 
 const Login = () => {
   const [loginSuccess, setLoginSuccess] = useState(false);
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -23,16 +23,7 @@ const registerValidationSchema = yup.object().shape({
     .required("*Campo Obligatorio"),
 });
 
-const caesarCipher = (str, shift) => {
-  return str.split('').map(char => {
-    if (char.match(/[a-z]/i)) {
-      const code = char.charCodeAt();
-      let base = code >= 65 && code <= 90 ? 65 : 97;
-      return String.fromCharCode(((code - base + shift) % 26) + base);
-    }
-    return char;
-  }).join('');
-};
+
 
 const Register = () => {
   const navigate = useNavigate();
